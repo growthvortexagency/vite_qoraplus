bd1e6a5705dfe45c2e883db5eafdfc66833a1e93
growthvortexagency <growthvortexagency@gmail.com>
2025-03-24 21:47:58 +0530

    Features section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import '../styles/pages/Home.css'
 import dashboard from '../assets/images/dashboard.png'
+import building from '../assets/icons/building-circle-check-solid.svg'
 
 const Home = () => {
     return (
@@ -12,7 +13,23 @@ const Home = () => {
                 <button>Get Started</button>
             </div>
             <div className='dashboard-image'>
-                <img src={dashboard} alt='dashboard'/>
+                <img src={dashboard} alt='dashboard' />
+            </div>
+            <div className='section-2'>
+                <div className='features'>Features</div>
+                <h1>Maximize Productivity with Our Cloud Software</h1>
+                <p>Stay ahead with our all-in-one solution for ERP, CRM, school/college management, 
+                    sales, and more. Automate processes, manage operations efficiently, and boost your 
+                    business growth—all from a single platform.</p>
+            </div>
+            <div className='features-cards'>
+                <div className='card'>
+                    <div className='icon'>
+                        <img src={building} alt='building-icon'/>
+                    </div>
+                    <h2>Business Management</h2>
+                    <p>Seamlessly handle daily operations across departments.</p>
+                </div>
             </div>
         </div>
     )
