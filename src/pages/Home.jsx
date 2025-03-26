8b00166f37de81605c8df1c6447d218f199accfb
growthvortexagency <growthvortexagency@gmail.com>
2025-03-26 22:13:17 +0530

    Features section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,46 @@
 import '../styles/pages/Home.css'
 import dashboard from '../assets/images/dashboard.png'
 import building from '../assets/icons/building-circle-check-solid.svg'
+import gear from '../assets/icons/gear-solid.svg'
+import chart from '../assets/icons/chart-simple-solid.svg'
+import hourglass from '../assets/icons/hourglass-start-solid.svg'
+import database from '../assets/icons/database-solid.svg'
+import link from '../assets/icons/link-solid.svg'
 
 const Home = () => {
+
+    const features = [
+        {
+            icon: building,
+            headline: "Business Management",
+            paragraph: "Seamlessly handle daily operations across departments."
+        },
+        {
+            icon: gear,
+            headline: "Task Automation",
+            paragraph: "Automate repetitive tasks and enhance efficiency."
+        },
+        {
+            icon: chart,
+            headline: "Multiple Dashboards",
+            paragraph: "Gain real-time insights with customizable views."
+        },
+        {
+            icon: hourglass,
+            headline: "Workflow Scheduling",
+            paragraph: "Organize tasks and set priorities effortlessly."
+        },
+        {
+            icon: database,
+            headline: "Centralized Data",
+            paragraph: "Keep all business records, leads, and reports in one place."
+        },
+        {
+            icon: link,
+            headline: "Smart Integrations",
+            paragraph: "Connect with your favorite tools for better workflow."
+        }
+    ]
     return (
         <div className='homepage-main-content'>
             <div className='section-1'>
@@ -18,18 +56,19 @@ const Home = () => {
             <div className='section-2'>
                 <div className='features'>Features</div>
                 <h1>Maximize Productivity with Our Cloud Software</h1>
-                <p>Stay ahead with our all-in-one solution for ERP, CRM, school/college management, 
-                    sales, and more. Automate processes, manage operations efficiently, and boost your 
+                <p>Stay ahead with our all-in-one solution for ERP, CRM, school/college management,
+                    sales, and more. Automate processes, manage operations efficiently, and boost your
                     business growth—all from a single platform.</p>
             </div>
             <div className='features-cards'>
-                <div className='card'>
+                {features.map((feature, index) => (<div key={index} className='card'>
                     <div className='icon'>
-                        <img src={building} alt='building-icon'/>
+                        <img src={feature.icon} alt='building-icon' />
                     </div>
-                    <h2>Business Management</h2>
-                    <p>Seamlessly handle daily operations across departments.</p>
+                    <h2>{feature.headline}</h2>
+                    <p>{feature.paragraph}</p>
                 </div>
+                ))}
             </div>
         </div>
     )
